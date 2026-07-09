1d405e265484451b1c85c2d7de59b14b51159f60
David Garcia Marchena <djgarcia.24@est.ucab.edu.ve>
2026-07-09 15:25:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
-// 1. Importamos las herramientas que compramos
 const express = require('express');
 const cors = require('cors');
 
-// 2. Inicializamos el servidor
+//inicializacion
 const app = express();
 app.use(cors()); // Permite que tu HTML se conecte aquí
 app.use(express.json()); // Permite leer la información de los formularios
 
-// 3. Tus usuarios manuales (Nuestra "Base de datos" por ahora)
+// usuarios
 const usuarios = [
     { email: "admin@correo.com", password: "123" },
     { email: "david@correo.com", password: "456" },
     { email: "invitado@correo.com", password: "789" }
 ];
 
-// 4. La ruta para iniciar sesión
+// permite el inicio de sesion
 app.post('/login', (req, res) => {
-    const emailRecibido = req.body.email;
-    const passwordRecibida = req.body.password;
-
-    // Buscamos si el correo existe en nuestra lista
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    //funcion que itera sobre los usuarios
     const usuarioEncontrado = usuarios.find(u => u.email === emailRecibido);
 
     if (!usuarioEncontrado) {
@@ -30,17 +29,11 @@ app.post('/login', (req, res) => {
         return res.status(401).json({ error: "Contraseña incorrecta." });
     }
 
-    // Si todo está bien, damos luz verde para que entren a buscar música
     return res.status(200).json({ mensaje: "¡Inicio de sesión exitoso!" });
 });
 
-// FUNCION SOLO PARA PROBAR LA PRIMERA VEZ SI FUNCIONA
-app.get('/usuario-uno', (req, res) => {
-    const usuario1 = usuarios[0]; // El primero de tu lista
-    res.json(usuario1);
-});
 
-// 5. Encendemos el servidor en el puerto 3000
+//encendido
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor encendido y escuchando en el puerto ${PORT}`);
